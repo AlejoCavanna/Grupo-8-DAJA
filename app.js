83a3ff06bb6848d974892681c790922d91a45b9e
Alejandro Bagnat <abagnat@yahoo.com.ar>
2022-04-14 20:09:56 -0300

    generamos archivos html y css

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ app.get("/",(req,res)=>{
     let htmlPath = path.resolve(__dirname,"./views/home.html");
     res.sendFile(htmlPath);
 });
+
+app.get("/producto",(req,res)=>{
+    let htmlPath = path.resolve(__dirname,"./views/productDetail.html");
+    res.sendFile(htmlPath);
+});
+app.get("/carrito",(req,res)=>{
+    let htmlPath = path.resolve(__dirname,"./views/productCart.html");
+    res.sendFile(htmlPath);
+});
+app.get("/registro",(req,res)=>{
+    let htmlPath = path.resolve(__dirname,"./views/register.html");
+    res.sendFile(htmlPath);
+});
+app.get("/login",(req,res)=>{
+    let htmlPath = path.resolve(__dirname,"./views/login.html");
+    res.sendFile(htmlPath);
+});
